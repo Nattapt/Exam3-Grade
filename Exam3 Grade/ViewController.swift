33f2028e6444b100df4485a3438fd0309c7cd32f
Nattapat <san.1234@hotmail.com>
2018-10-12 14:54:26 +0700

    Initial Commit

---
+++ Exam3 Grade/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exam3 Grade
+//
+//  Created by Nattapat on 12/10/2561 BE.
+//  Copyright © 2561 Nattapat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
